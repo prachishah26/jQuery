5c347a8c44972928dd0f4ce21bee5c8a7a7abb5c
Prachi Shah <prachishah0567@gmail.com>
2022-05-20 14:38:33 +0530

    task

--- Task-2/js/script.js
+++ Task-2/js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-    $("p").hide().first().show()
-    $(".up").hide().first().show()
-    $(".down").first().hide()
+    $("p").hide().first().show(); //this will keep first paragraph open at startng
+    $(".up").hide().first().show(); //arrow signs at start
+    $(".down").first().hide();
 
-    $(".btn").click(function(){
-        $(this).parent().siblings().find("p").slideUp()
-        $(this).parent().siblings().find(".down").show()
-        $(this).parent().siblings().find(".up").hide()
+    $(".btn").click(function(){                   //on button click...
+        $(this).parent().siblings().find("p").slideUp();  
+        $(this).parent().siblings().find(".down").show(); 
+        $(this).parent().siblings().find(".up").hide();
 
-        $(this).next().toggle()
-        $(this).find("i").toggle()
-    })
-})
+        $(this).next().toggle();  
+        $(this).find("i").toggle(); 
+    });
+});
