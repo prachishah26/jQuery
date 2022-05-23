79fca214a0fcab253bc0e01ee4e5a9c9afd08b31
Prachi Shah <prachishah0567@gmail.com>
2022-05-23 13:56:13 +0530

    task

---
+++ Task-3/js/script.js
@@ -0,0 +1,51 @@
+function icon_color(){
+    var i = $("img:visible").index();
+        $(".slide-icon").css("color", "white").eq(i).css("color", "red");
+        $(".heading h4").hide().eq(i).show()
+}
+
+
+$(document).ready(function () {
+    var index = 0;
+    var start = setInterval(function(){
+        $("img").hide().eq(index).show().fadeOut(5000);
+        icon_color();
+        index += 1;
+        if(index > $("img").last().index()){
+            index = 0;
+        }
+    },3000)
+    start;
+
+    $("img").hide().eq(0).show();
+    $(".left-arrow").click(function () {
+        clearInterval(start);
+        $("img").hide().eq(index - 1).show();
+        index -= 1;
+        if (index <= $("img").first().index()) {
+            index = $("img").last().index() + 1;
+        }
+        icon_color();
+    })
+
+    $(".right-arrow").click(function () {
+        clearInterval(start);
+        $("img").hide().eq(index + 1).show();
+        index += 1;
+        if (index >= $("img").last().index()) {
+            index = -1;
+        }
+        icon_color();
+        
+    })
+    $(".slide-icon").click(function () {
+        clearInterval(start);
+        $(this).css("color", "red")
+        $(this).siblings().css("color", "white");
+        var icon = $(this).index()
+        $("img").hide().eq(icon).show()
+        $(".heading h4").hide().eq(icon).show()
+    })
+
+})
+
