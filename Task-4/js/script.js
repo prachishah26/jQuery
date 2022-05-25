1408d0b6921a0a7af3a87ee88a8b2d056065673c
Prachi Shah <prachishah0567@gmail.com>
2022-05-25 09:47:56 +0530

    added comments

--- Task-4/js/script.js
+++ Task-4/js/script.js
@@ -1,19 +1,18 @@
-
 $(document).ready(function () {
     $(".resume").hide();
     $(".restart").hide();
 
     $(".start").click(function () {
         if ($(".enter-time").val() == 0) {
-            $(".time-status").text("Please enter seconds first !!!")
+            $(".time-status").text("Please enter seconds first !!!");
         }
         else {
-            $(this).attr('disabled','disabled');
-            $(".pause").attr("disabled",false);
-            $(".time-status").addClass("green").removeClass("red").removeClass("blue").removeClass("yello");
-            $(".status").addClass("green-border").removeClass("red-border").removeClass("blue-border").removeClass("yello-border");
+            $(this).attr('disabled', 'disabled');
+            $(".pause").attr("disabled", false);
+            $(".time-status").addClass("green").removeClass("red blue yello");
+            $(".status").addClass("green-border").removeClass("red-border blue-border yello-border");
 
-            $(".time-status").text("Running............ !!!")
+            $(".time-status").text("Running............ !!!");
             var sec = parseInt($(".enter-time").val());
 
             var hours = Math.floor(sec / 3600);
@@ -24,23 +23,23 @@ $(document).ready(function () {
             $(".hours").text(hours);
             $(".minutes").text(minutes);
             $(".seconds").text(seconds);
-            $(".c_seconds").text(c_seconds)
+            $(".c_seconds").text(c_seconds);
 
-            countdown();
+            countdown(); //start the countdown..
         }
     })
 
     $(".resume").click(function () {
-        $(this).attr('disabled','disabled');
-        $(".time-status").text("Running............ !!!")
+        $(this).attr('disabled', 'disabled');
+        $(".time-status").text("Running... !!!")
         resume();
     })
 
     $(".restart").click(function () {
         $(this).hide();
         $(".start").show();
-        $(".start").attr("disabled",false);
-        $(".time-status").text("Enter time and hit start !")
+        $(".start").attr("disabled", false);
+        $(".time-status").text("Enter time and hit start !");
         $(".time-status").addClass("yello").removeClass("red");
         $(".status").addClass("yello-border").removeClass("red-border");
         restart();
@@ -48,14 +47,14 @@ $(document).ready(function () {
 
     $(".pause").click(function () {
         if ($(".enter-time").val() == 0) {
-            $(".time-status").text("Please enter seconds first !!!")
+            $(".time-status").text("Please enter seconds first !!!");
         }
         else {
             $(".start").hide();
             $(".resume").show();
-            $(".resume").attr("disabled",false);
-            $(".time-status").addClass("blue").removeClass("green").removeClass("red");
-            $(".status").addClass("blue-border").removeClass("green-border").removeClass("blue-border");
+            $(".resume").attr("disabled", false);
+            $(".time-status").addClass("blue").removeClass("green red");
+            $(".status").addClass("blue-border").removeClass("green-border blue-border");
 
             $(".time-status").text("Paused !")
             pause();
@@ -71,9 +70,9 @@ $(document).ready(function () {
             $(".time-status").text("Stopped !")
             $(".resume").hide();
             $(".restart").show();
-            $(".pause").attr("disabled",true);
-            $(".time-status").addClass("red").removeClass("green").removeClass("blue");
-            $(".status").addClass("red-border").removeClass("green-border").removeClass("blue-border");
+            $(".pause").attr("disabled", true);
+            $(".time-status").addClass("red").removeClass("green blue");
+            $(".status").addClass("red-border").removeClass("green-border blue-border");
             stop();
         }
     })
@@ -81,10 +80,10 @@ $(document).ready(function () {
     $(".reset").click(function () {
         $(".time-status").text("Enter time and hit start !")
         $(".restart, .resume").hide();
-        $(".start").attr("disabled",false);
-        $(".pause").attr('disabled',false);
-        $(".time-status").addClass("gray").removeClass("red").removeClass("blue").removeClass("green");
-            $(".status").addClass("gray-border").removeClass("red-border").removeClass("blue-border").removeClass("green-border");
+        $(".start").attr("disabled", false);
+        $(".pause").attr('disabled', false);
+        $(".time-status").addClass("gray").removeClass("red blue green");
+        $(".status").addClass("gray-border").removeClass("red-border blue-border green-border");
         reset();
     })
     var start;
@@ -116,7 +115,7 @@ $(document).ready(function () {
                             seconds = 0;
                             c_seconds = 0;
                             clearInterval(start);
-                            $(".time-status").text("Your time is finished now !!!!")
+                            $(".time-status").text("Your time is finished now !!!!");
 
                         }
                     }
@@ -130,7 +129,7 @@ $(document).ready(function () {
         }, 10)
         return hours, minutes, seconds, c_seconds;
     }
-    function values(){
+    function values() {
         // this function will change the values of time in respective place 
         $(".c-seconds").text(c_seconds);
         $(".seconds").text(seconds);
@@ -139,12 +138,14 @@ $(document).ready(function () {
     }
 
     function pause() {
+        // this function will pause the timer 
         clearInterval(start);
         values();
-        $(".time-status").text(`Paused at ${hours} Hours , ${minutes} Minutes, ${seconds} Seconds`)
+        $(".time-status").text(`Paused at ${hours} Hours , ${minutes} Minutes, ${seconds} Seconds`);
     }
 
     function stop() {
+        // this function will stop the timer and show restart button
         clearInterval(start);
         values();
         $(".restart").show();
@@ -153,27 +154,32 @@ $(document).ready(function () {
     }
 
     function reset() {
+        // this function will reset the timer and start from over 
         clearInterval(start);
-        $(".start").show()
-        $(".c-seconds").text(0);
-        $(".seconds").text(0);
-        $(".minutes").text(0);
-        $(".hours").text(0);
+        $(".start").show();
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+        c_seconds = 0;
+        values();
         $(".enter-time").val(null);
     }
 
     function resume() {
+        // this function will resume the timer 
         clearInterval(start);
         values();
         countdown();
     }
 
     function restart() {
+        // this function will restart the timer 
         clearInterval(start);
-        $(".c-seconds").text(0);
-        $(".seconds").text(0);
-        $(".minutes").text(0);
-        $(".hours").text(0);
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+        c_seconds = 0;
+        values();
         $(".enter-time").val(null);
     }
 })
