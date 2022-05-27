0d7dd8eee6251726c1b694c3998f00a4c4b6f08b
Prachi Shah <prachishah0567@gmail.com>
2022-05-27 12:07:22 +0530

    .

--- Task-6/js/script.js
+++ Task-6/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("table").hide();
+    $("table").hide()
     var index,page,firstName,lastName,gender,email,contact,dob,sports,about,checkbox,current_tr;
 
     $(".step").click(function () {
@@ -17,31 +17,26 @@ $(document).ready(function () {
         $(".level").hide().eq(page - 1).show();
     });
 
-
     // submit button 
     $(".submit").click(function (e) {
-        e.preventDefault();
+        e.preventDefault()
         get_details();
         clear_details();
-        
+
         $("table").show();
-        $(".data-title").show();
         var count = $('#myTable tr').length;
 
-        $(".data-table tbody").append("<tr data-fname='"+firstName+"' data-lname='" +lastName + "'data-gender='" + gender + "'data-email='" + email + "'data-contact='" + contact + "'data-dob='"+ dob + "'data-sports='" + sports +"'data-about='" + about +"'><td>"+count+ "</td><td>"+firstName + "</td><td>"+lastName+"</td><td>"+gender+"</td><td>"+email+"</td><td>"+contact+"</td><td>"+dob+"</td><td>"+sports+"</td><td>"+about+"</td><td>"+checkbox+"</td><td>"+'<button class="btn btn-info btn-edit">edit</button>' +"</td><td>"+'<button class="btn btn-danger btn_delete">DELETE</button>'+"</td></tr>");
+
+        $(".data-table tbody").append("<tr data-fname='"+firstName+"' data-lname='" +lastName + "'data-gender='" + gender + "'data-email='" + email + "'data-contact='" + contact + "'data-dob='"+ dob + "'data-sports='" + sports +"'data-about='" + about +"'><td>"+count+ "</td><td>"+firstName + "</td><td>"+lastName+"</td><td>"+gender+"</td><td>"+email+"</td><td>"+contact+"</td><td>"+dob+"</td><td>"+sports+"</td><td>"+about+"</td><td>"+'<button class="btn btn-info btn-edit">edit</button>' +"</td><td>"+'<button class="btn btn-danger btn_delete">DELETE</button>'+"</td></tr>");
 
     })
 
-    // delete button 
     $("body").on("click",".btn_delete",function(){
         $(this).parents("tr").remove();
 
         $("tbody tr").each(function(index){
             $(this).find("td:eq(0)").text(index+1);
         })
-        if ($("tbody tr").length == 0){
-            $("table, .data-title").hide();
-        }
     })
 
     // edit button 
@@ -73,22 +68,33 @@ $(document).ready(function () {
     })
     $("body").on("click",".btn-cancel",function(){
         clear_details();
+
         $(".btn-update, .btn-cancel").hide();
         $(".submit").show()
     })
-     
+
     // update button 
     $("body").on("click",".btn-update",function(){
         $(".level").hide().first().show();
         var count = $('#myTable tr').length-1;
-        $(this).hide();
-
+        $(this).hide();$(".btn-cancel").hide();
+        (".submit").show();
         get_details();
-        clear_details();
-        
+
+        $("input[type=text],input[type=phone], textarea").val("");
+        $('input[name="gridRadios"]').prop('checked', false);
+        $("#sports").val(0);
+
         $("tbody tr").eq(current_tr).replaceWith("<tr data-fname='"+firstName+"' data-lname='" +lastName + "'data-gender='" + gender + "'data-email='" + email + "'data-contact='" + contact + "'data-dob='"+ dob + "'data-sports='" + sports +"'data-about='" + about +"'><td>"+count+ "</td><td>"+firstName + "</td><td>"+lastName+"</td><td>"+gender+"</td><td>"+email+"</td><td>"+contact+"</td><td>"+dob+"</td><td>"+sports+"</td><td>"+about+"</td><td>"+'<button class="btn btn-info btn-edit">edit</button>' +"</td><td>"+'<button class="btn btn-danger btn_delete">DELETE</button>'+"</td></tr>");
     })
 
+    function clear_details(){
+        $("input[type=text],input[type=phone], textarea").val("");
+        $('input[name="gridRadios"]').prop('checked', false);
+        $("#sports").val(0);
+        $('input[type="checkbox"]').prop('checked',false);
+        $(".level").hide().first().show();
+    }
     function get_details(){
         firstName = $("#fname").val();
         lastName = $("#lname").val();
@@ -105,7 +111,6 @@ $(document).ready(function () {
         if ($("#sports").val()==0){
             sports = ""
         }
-        
         about = $("#about").val();
         if ($(':checkbox').is(':checked')){
             checkbox = "Yes";
@@ -114,11 +119,4 @@ $(document).ready(function () {
             checkbox = "No";
         }
     }
-    function clear_details(){
-        $("input[type=text],input[type=phone], textarea").val("");
-        $('input[name="gridRadios"]').prop('checked', false);
-        $("#sports").val(0);
-        $('input[type="checkbox"]').prop('checked',false);
-        $(".level").hide().first().show();
-    }
-})
+}) 
