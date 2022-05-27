36c2f05a3756bb72a264e60785fe25193cab3a44
Prachi Shah <prachishah0567@gmail.com>
2022-05-27 11:26:37 +0530

    task

--- Task-6/js/script.js
+++ Task-6/js/script.js
@@ -1,16 +1,6 @@
 $(document).ready(function () {
-    $("table").hide()
-    var index = 0;
-    var page;
-    var firstName;
-    var lastName;
-    var gender;
-    var email;
-    var contact;
-    var dob;
-    var sports;
-    var about;
-    var current_tr;
+    $("table").hide();
+    var index,page,firstName,lastName,gender,email,contact,dob,sports,about,checkbox,current_tr;
 
     $(".step").click(function () {
         index = $(this).index();
@@ -27,38 +17,31 @@ $(document).ready(function () {
         $(".level").hide().eq(page - 1).show();
     });
 
+
     // submit button 
     $(".submit").click(function (e) {
-        e.preventDefault()
-        firstName = $("#fname").val();
-        lastName = $("#lname").val();
-        gender = $("input[type=radio][name=gridRadios]:checked").val();
-        email = $("#email").val();
-        contact = $("#contact").val();
-        dob = $("#birthday").val();
-        sports = $("#sports").val();
-        about = $("#about").val();
-
-        $("input[type=text],input[type=phone], textarea").val("");
-        $('input[name="gridRadios"]').prop('checked', false);
-        $("#sports").val(0);
-        $(".level").hide().first().show();
-
+        e.preventDefault();
+        get_details();
+        clear_details();
         
         $("table").show();
+        $(".data-title").show();
         var count = $('#myTable tr').length;
-        
 
-        $(".data-table tbody").append("<tr data-fname='"+firstName+"' data-lname='" +lastName + "'data-gender='" + gender + "'data-email='" + email + "'data-contact='" + contact + "'data-dob='"+ dob + "'data-sports='" + sports +"'data-about='" + about +"'><td>"+count+ "</td><td>"+firstName + "</td><td>"+lastName+"</td><td>"+gender+"</td><td>"+email+"</td><td>"+contact+"</td><td>"+dob+"</td><td>"+sports+"</td><td>"+about+"</td><td>"+'<button class="btn btn-info btn-edit">edit</button>' +"</td><td>"+'<button class="btn btn-danger btn_delete">DELETE</button>'+"</td></tr>");
+        $(".data-table tbody").append("<tr data-fname='"+firstName+"' data-lname='" +lastName + "'data-gender='" + gender + "'data-email='" + email + "'data-contact='" + contact + "'data-dob='"+ dob + "'data-sports='" + sports +"'data-about='" + about +"'><td>"+count+ "</td><td>"+firstName + "</td><td>"+lastName+"</td><td>"+gender+"</td><td>"+email+"</td><td>"+contact+"</td><td>"+dob+"</td><td>"+sports+"</td><td>"+about+"</td><td>"+checkbox+"</td><td>"+'<button class="btn btn-info btn-edit">edit</button>' +"</td><td>"+'<button class="btn btn-danger btn_delete">DELETE</button>'+"</td></tr>");
 
     })
 
+    // delete button 
     $("body").on("click",".btn_delete",function(){
         $(this).parents("tr").remove();
 
         $("tbody tr").each(function(index){
             $(this).find("td:eq(0)").text(index+1);
         })
+        if ($("tbody tr").length == 0){
+            $("table, .data-title").hide();
+        }
     })
 
     // edit button 
@@ -89,10 +72,7 @@ $(document).ready(function () {
 
     })
     $("body").on("click",".btn-cancel",function(){
-        $("input[type=text],input[type=phone], textarea").val("");
-        $('input[name="gridRadios"]').prop('checked', false);
-        $("#sports").val(0);
-
+        clear_details();
         $(".btn-update, .btn-cancel").hide();
         $(".submit").show()
     })
@@ -102,19 +82,43 @@ $(document).ready(function () {
         $(".level").hide().first().show();
         var count = $('#myTable tr').length-1;
         $(this).hide();
+
+        get_details();
+        clear_details();
+        
+        $("tbody tr").eq(current_tr).replaceWith("<tr data-fname='"+firstName+"' data-lname='" +lastName + "'data-gender='" + gender + "'data-email='" + email + "'data-contact='" + contact + "'data-dob='"+ dob + "'data-sports='" + sports +"'data-about='" + about +"'><td>"+count+ "</td><td>"+firstName + "</td><td>"+lastName+"</td><td>"+gender+"</td><td>"+email+"</td><td>"+contact+"</td><td>"+dob+"</td><td>"+sports+"</td><td>"+about+"</td><td>"+'<button class="btn btn-info btn-edit">edit</button>' +"</td><td>"+'<button class="btn btn-danger btn_delete">DELETE</button>'+"</td></tr>");
+    })
+
+    function get_details(){
         firstName = $("#fname").val();
         lastName = $("#lname").val();
-        gender = $("input[type=radio][name=gridRadios]:checked").val();
+        if($("input[type=radio][name=gridRadios]").is(':checked')){
+            gender = $("input[type=radio][name=gridRadios]:checked").val()
+        }
+        else{
+            gender = "";
+        }
         email = $("#email").val();
         contact = $("#contact").val();
         dob = $("#birthday").val();
         sports = $("#sports").val();
+        if ($("#sports").val()==0){
+            sports = ""
+        }
+        
         about = $("#about").val();
-
+        if ($(':checkbox').is(':checked')){
+            checkbox = "Yes";
+        }
+        else{
+            checkbox = "No";
+        }
+    }
+    function clear_details(){
         $("input[type=text],input[type=phone], textarea").val("");
         $('input[name="gridRadios"]').prop('checked', false);
         $("#sports").val(0);
-
-        $("tbody tr").eq(current_tr).replaceWith("<tr data-fname='"+firstName+"' data-lname='" +lastName + "'data-gender='" + gender + "'data-email='" + email + "'data-contact='" + contact + "'data-dob='"+ dob + "'data-sports='" + sports +"'data-about='" + about +"'><td>"+count+ "</td><td>"+firstName + "</td><td>"+lastName+"</td><td>"+gender+"</td><td>"+email+"</td><td>"+contact+"</td><td>"+dob+"</td><td>"+sports+"</td><td>"+about+"</td><td>"+'<button class="btn btn-info btn-edit">edit</button>' +"</td><td>"+'<button class="btn btn-danger btn_delete">DELETE</button>'+"</td></tr>");
-    })
+        $('input[type="checkbox"]').prop('checked',false);
+        $(".level").hide().first().show();
+    }
 })
