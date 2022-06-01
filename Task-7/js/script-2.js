73450b0a36db603e97d03b5bcdf727e26af53fd5
Prachi Shah <prachishah0567@gmail.com>
2022-06-01 19:06:08 +0530

    task

--- Task-7/js/script-2.js
+++ Task-7/js/script-2.js
@@ -1,77 +1,92 @@
-$(document).ready(function () {
+$(document).ready(function() {
     //Adding to the expression
-    var equation, status;
-    $(".btn").click(function () {
-        if (status == true) {
+    var equation=0,status=false;
+    $(".btn").click(function() {
+        if(status == true){
             $("#expression").val(0);
+            equation = 0;
             status = false;
         }
         if (!$(this).hasClass("not")) {
-            if ($("#expression").val() == 0)
+            if ($("#expression").val() == 0 && equation ==0){
                 $("#expression").val($(this).text());
-            else {
+                equation +=  $(this).text();
+            }
+            else{
                 // console.log($(this).text());
-
-                if ($(this).html() === '<i class="fa-solid fa-square-root-variable"></i>') {
+                
+                if ($(this).html() === '<i class="fa-solid fa-square-root-variable"></i>'){
                     var sqrt = $("#expression").val();
-                    $("#expression").val($("#expression").val() + "**(0.5)");
-
+                   
+                    // equation += (sqrt + "**(0.5)" );
+                    equation =  sqrt + "**(0.5)";
+                    $("#expression").val($("#expression").val() + "sqrt");
+                    // console.log(equation)
+                    
                 }
-                if ($(this).text() === "x2") {
-                    // var square = $("#expression").val();
-                    $("#expression").val($("#expression").val() + "**2");
+                if ($(this).text() === "x2"){
+                    var square = $("#expression").val();
+                    // equation += (square + "**2" );
+                    equation = sqrt + "**2";
+                    $("#expression").val($("#expression").val() + "^2");
                 }
-                else {
-                    $("#expression").val($("#expression").val() + $(this).text());
+                else{
+                    equation = $("#expression").val($("#expression").val() + $(this).text());
+                    // $("#expression").val($("#expression").val() + $(this).text());
                 }
             }
         }
     });
     //Backspace
-    $('.back').click(function () {
+    $('.back').click(function() {
         var value = $("#expression").val();
+        var value2 = equation
         if (!(parseInt(parseFloat(value)) == 0 && value.length == 1))
             $("#expression").val(value.slice(0, value.length - 1));
         if (value.length == 1)
             $("#expression").val("0");
+
+        if (!(parseInt(parseFloat(value2)) == 0 && value2.length == 1))
+            $("#expression").val(value2.slice(0, value2.length - 1));
+        if (value2.length == 1)
+            $("#expression").val("0");
     });
 
     // clear button-----------------------------------
-    $(".clear").click(function () {
+    $(".clear").click(function() {
         $("#expression").val("0");
-        status = true;
+        equation = 0
     });
 
     // equal button ----------------------------------
-    $(".equal").click(function () {
+    $(".equal").click(function() {
         status = true;
         var result;
         //Check for syntax error----------------------
         try {
-            if (eval(($("#expression").val()))!==Infinity){
-                result = (eval(($("#expression").val())));
-            }
-            
+            result = (eval(equation));
         } catch (e) {
             if (e instanceof SyntaxError) {
                 alert("Error! Resetting values.");
                 $("#expression").val("0");
+                equation = 0
                 $(".result").val("0");
             }
             if (e instanceof TypeError) {
                 alert("Error! Resetting values.");
                 $("#expression").val("0");
+                equation = 0
                 $(".result").val("0");
             }
         }
         var contain = $("#expression").val()
-        if ($("contain:contains('x2')")) {
+        if ($("contain:contains('x2')")){
             $(".history").append(`<div class="d-flex"><p>${$("#expression").val().replace("**2", "^2")}</p><p>${'='}</p><p>${result}</p></div>`)
-            $(".result").val(result);
+        $(".result").val(result);
         }
-        else {
-            $(".history").append(`<div class="d-flex"><p>${$("#expression").val()}</p><p>${'='}</p><p>${result}</p></div>`)
-            $(".result").val(result);
+        else{
+        $(".history").append(`<div class="d-flex"><p>${$("#expression").val()}</p><p>${'='}</p><p>${result}</p></div>`)
+        $(".result").val(result);
         }
         result = 0;
     });
@@ -82,12 +97,10 @@ $(document).ready(function () {
             status = false;
         }
         $(".result").val($(".result").val() + (String.fromCharCode(e.which)));
-
         var key = e.which;
         if (key == 13) { // the enter key code
             $('.equal').click();
         }
-
     });
     $(document).keyup(function (e) {
         var key = e.which;
