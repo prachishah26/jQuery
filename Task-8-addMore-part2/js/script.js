680aa1a85a65ce3dfaddc807f83b822ca9e88e35
Prachi Shah <prachishah0567@gmail.com>
2022-06-06 18:02:06 +0530

    task

--- Task-8-addMore-part2/js/script.js
+++ Task-8-addMore-part2/js/script.js
@@ -1,32 +1,83 @@
 $(document).ready(function () {
-    var count = 0, number = 0, nameOfbox, num = 1, isDataExists = false;
+    var count = 0, nameOfbox, counter = 1, isDataExists = false,input;
+    $(".btn-delete").attr("disabled", true);
 
     // This button will add another box of details -------------- 
     $(".addMore").click(function () {
-
-        $(".allBoxes").append(`<div class="box" name=${num}><input type="text" class="p-2" placeholder="title"><button class="btn btn-secondary p-2 addChild">Add Child</button><button class="btn btn-danger btn-delete">Delete</button></div>`)
-        ++num;
+        $(".btn-delete").attr("disabled", false);
+        $(".allBoxes").append(`<div class="box" name=${counter}><input type="text" class="p-2 text" placeholder="title"><button class="btn btn-secondary p-2 addChild">Add Child</button><button class="btn btn-danger btn-delete">Delete</button></div>`)
+        ++counter;
     })
 
     // This button will add child -------------------------------- 
     $("body").on("click", ".addChild", function () {
-
-        $(this).parent().append(`<div class="details"><input type="text" placeholder="Subtitle" class="p-2 me-2"><input type="text" placeholder="Value" class="p-2 me-2">
+        $(this).parent().append(`<div class="details"><input type="text " placeholder="Subtitle" class="p-2 subtitle me-2"><input type="text " placeholder="Value" class="p-2 value me-2">
             <button class="btn btn-danger delete">Delete</button></div>`)
     })
 
-    // This button will delete the child --------------------------
+    // This button will delete the child ---------------------
     $("body").on("click", ".delete", function () {
-        $(this).parent().remove();
+        var parent = $(this).parent()
+        var boxIndex = $(this).parents(".box").index()
+        var parentIndex = $(this).parent().index();
+        bootbox.confirm({
+            message: "Do you want to delete this child element?",
+            buttons: {
+                cancel: {
+                    label: '<i class="fa fa-times"></i> Cancel'
+                },
+                confirm: {
+                    label: '<i class="fa fa-check"></i> Confirm'
+                }
+            },
+            callback: function (result) {
+                if (result) {
+                    parent.remove();
+                    
+                    console.log($(".record>div").eq(boxIndex-1).find("p").eq(parentIndex));
+                    $(".record>div").eq(boxIndex-1).find("p").eq(parentIndex-2).remove();
+                    $(".record>div").eq(boxIndex-1).find("p").eq(parentIndex-3).remove();
+                }
+            }
+        });
     })
 
-    // submit button -----------------------------------------------
-    $("body").on("click", ".fill", function () {
-        nameOfbox = $(this).parent().attr("name");
-        inp = $(this).parent().find("input");
+    //This will delete the parent element 
+    $(document).on("click", ".btn-delete", function () {
+
+        var parent = $(this).parent()
+        var parentIndex = $(this).parent().index();
+
+        if ($(".allBoxes > div").length == 1) {
+            $(".btn-delete").attr("disabled", true)
+        }
+        else {
+            bootbox.confirm({
+                message: "Do you want to delete this ?",
+                buttons: {
+                    confirm: {
+                        label: 'Yes',
+                        className: 'btn-success'
+                    },
+                    cancel: {
+                        label: 'No',
+                        className: 'btn-danger'
+                    }
+                },
+                callback: function (result) {
+                    if (result) {
+                        parent.remove();
+                        $(".record>div").eq(parentIndex - 1).remove()
+                    }
+                }
+            })
+        };
+    });
+    $(document).on("keyup", ".text,.subtitle, .value", function () {
+        nameOfbox = $(this).parents(".box").attr("name");
+        input = $(this).parents(".box").find("input");
 
         if ($(".record>div").length > 0) {
-
             // this will match input's name attribute and specification's div's data attribute. if match then it will replace the content while submit 
             $(".record").find("div").each(function (index, e) {
                 if ($(e).data("sort") === parseInt(nameOfbox)) {
@@ -36,7 +87,7 @@ $(document).ready(function () {
                         return $(this).data("sort") == nameOfbox
                     }).empty();
 
-                    inp.each(function (index, element) {
+                    input.each(function (index, element) {
                         if (index == 0) {
                             $(".record").find("div").filter(function () {
                                 return $(this).data("sort") == nameOfbox
@@ -54,9 +105,8 @@ $(document).ready(function () {
 
             // if not match then create new 
             if (isDataExists == false) {
-                console.log("false")
                 $(".record").append(`<div data-sort=${nameOfbox}></div>`)
-                inp.each(function (index, element) {
+                input.each(function (index, element) {
                     if (index == 0) {
                         $(".record").find("div").filter(function () {
                             return $(this).data("sort") == nameOfbox
@@ -70,8 +120,8 @@ $(document).ready(function () {
                 })
             }
         }
+
         else {
-            console.log("last else")
             $(".record").append(`<div data-sort=${nameOfbox}></div>`)
             $(this).parent().find("input").each(function (index, element) {
                 if (index == 0) {
@@ -92,9 +142,8 @@ $(document).ready(function () {
             }).appendTo(".record");
         }
         isDataExists = false
-    })
-})
-
+    });
+});
 
 
 
