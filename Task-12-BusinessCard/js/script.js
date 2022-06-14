e29373124669f1e54831be50ed3f0d5bf714d99e
Prachi Shah <prachishah0567@gmail.com>
2022-06-14 11:18:40 +0530

    task

--- Task-12-BusinessCard/js/script.js
+++ Task-12-BusinessCard/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     // global variables 
-    var text, value, className, document1, source, data, colorChange, colorvalue, section, page;
+    var text, data, colorvalue;
 
+    // wizard area show at start 
     $(".area").hide().first().show();
     $(".standard").hide();
 
@@ -16,16 +17,12 @@ $(document).ready(function () {
         if ($(this).attr("value") == "clean") {
             $(".cleanCard").show();
             $(".standardCard").hide();
-            $("#lightColor").val("#FFFFFF");
-            $("#mainColor").val("#000000");
-            $("#darkColor").val("#FFFFFF");
+            colorParameters()
+
         }
         if ($(this).attr("value") == "standard") {
             $(".cleanCard").hide();
             $(".standardCard").show();
-            // $(".standardCard .frontCard").css("background-color", "#87CEEB")
-            // $(".standardCard .frontCard,.backCard").css("color", "#000000");
-            // $(".standardCard .backCard").css("background-color", "#4B0082");
         }
     });
 
@@ -180,6 +177,7 @@ $(document).ready(function () {
         $(".backCard").html(`${img} </br> <p class="text-center">Scan me</p>`);
     })
 
+    // frontcard background color
     $("#lightColor").click(function () {
         let colorValue = setInterval(() => {
             let color = $("#lightColor").val();
@@ -188,6 +186,7 @@ $(document).ready(function () {
         }, 200);
     })
 
+    // font color 
     $("#mainColor").click(function () {
         colorValue = setInterval(() => {
             let color = $("#mainColor").val();
@@ -196,6 +195,7 @@ $(document).ready(function () {
         }, 200);
     })
 
+    // back card background color 
     $("#darkColor").click(function () {
         colorValue = setInterval(() => {
             let color = $("#darkColor").val();
@@ -204,82 +204,90 @@ $(document).ready(function () {
         }, 200);
     })
 
+    // on reset button click even => colors will set by default 
     $(".reset").click(function () {
-        clearInterval(colorvalue);
-        $("#lightColor").val("#FFFFFF");
-        $("#mainColor").val("000000");
-        $("#darkColor").val("#FFFFFF");
-        if ($('input[type="radio"]').attr("value") == "clean") {
-            clearInterval(colorvalue)
-            $(".cleanCard .frontCard").css("background-color", "#FFFFFF")
-            $(".cleanCard .frontCard,.backCard").css("color", "#000000");
-            $(".cleanCard .backCard").css("background-color", "#FFFFFF");
-        }
-        else {
-            $(".standardCard .frontCard").css("background-color", "#FFFFFF")
-            $(".standardCard .frontCard,.backCard").css("color", "#000000");
-            $(".standardCard .backCard").css("background-color", "#FFFFFF");
-        }
-
+        colorParameters();
+        
     })
 
     $.validator.setDefaults({ ignore: [] });
 
+    // on click download button click event 
     $(".btn-download").click(function (e) {
         if ($(".form").valid()) {
             Convert_HTML_To_PDF()
         }
-
         else {
-            console.log("download is pressed");
             $(".area").hide();
             $(".personalization").show();
         }
         e.preventDefault();
+        $('.form').trigger("reset");
+        colorParameters();
+        defaultName();
     })
-})
 
+    // by default color parameters 
+    function colorParameters() {
+        $("#lightColor").val("#FFFFFF");
+        $("#mainColor").val("000000");
+        $("#darkColor").val("#FFFFFF");
+    }
 
-function Convert_HTML_To_PDF() {
-    var elementHTML = document.getElementById('business-card');
+    // on submit the data will be remove 
+    function defaultName(){
+        $(".email").text("abc@gmail.com")
+        $(".fname").text("Person Name")
+        $(".designation").text("Designation")
+        $(".contact").text("1111111111")
+        $(".website").text("https://www.google.com/")
+        $(".companyName").text("Company Name")
+        $(".company-logo i").removeClass()
+        $(".company-logo i").addClass("fa-solid fa-bolt")
+        data = "Please enter URL"
+    }
 
-    html2canvas(elementHTML, {
-        useCORS: true,
-        onrendered: function (canvas) {
-            var pdf = new jsPDF('p', 'pt', 'letter');
+    function Convert_HTML_To_PDF() {
+         // pdf download function 
+        var elementHTML = document.getElementById('business-card');
+        html2canvas(elementHTML, {
+            useCORS: true,
+            onrendered: function (canvas) {
+                var pdf = new jsPDF('p', 'pt', 'letter');
 
-            var pageHeight = 980;
-            var pageWidth = 900;
-            for (var i = 0; i <= elementHTML.clientHeight / pageHeight; i++) {
-                var srcImg = canvas;
-                var sX = 0;
-                var sY = pageHeight * i; // start 1 pageHeight down for every new page
-                var sWidth = pageWidth;
-                var sHeight = pageHeight;
-                var dX = 0;
-                var dY = 0;
-                var dWidth = pageWidth;
-                var dHeight = pageHeight;
+                var pageHeight = 980;
+                var pageWidth = 900;
+                for (var i = 0; i <= elementHTML.clientHeight / pageHeight; i++) {
+                    var srcImg = canvas;
+                    var sX = 0;
+                    var sY = pageHeight * i; // start 1 pageHeight down for every new page
+                    var sWidth = pageWidth;
+                    var sHeight = pageHeight;
+                    var dX = 0;
+                    var dY = 0;
+                    var dWidth = pageWidth;
+                    var dHeight = pageHeight;
 
-                window.onePageCanvas = document.createElement("canvas");
-                onePageCanvas.setAttribute('width', pageWidth);
-                onePageCanvas.setAttribute('height', pageHeight);
-                var ctx = onePageCanvas.getContext('2d');
-                ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
+                    window.onePageCanvas = document.createElement("canvas");
+                    onePageCanvas.setAttribute('width', pageWidth);
+                    onePageCanvas.setAttribute('height', pageHeight);
+                    var ctx = onePageCanvas.getContext('2d');
+                    ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
 
-                var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
-                var width = onePageCanvas.width;
-                var height = onePageCanvas.clientHeight;
+                    var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
+                    var width = onePageCanvas.width;
+                    var height = onePageCanvas.clientHeight;
 
-                if (i > 0) // if we're on anything other than the first page, add another page
-                    pdf.addPage(612, 864); // 8.5" x 12" in pts (inches*72)
+                    if (i > 0) // if we're on anything other than the first page, add another page
+                        pdf.addPage(612, 864); // 8.5" x 12" in pts (inches*72)
 
-                pdf.setPage(i + 1); // now we declare that we're working on that page
-                pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width * .62), (height * .62)); // add content to the page
+                    pdf.setPage(i + 1); // now we declare that we're working on that page
+                    pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width * .62), (height * .62)); // add content to the page
+                }
+
+                // Save the PDF
+                pdf.save('Business-card.pdf');
             }
-
-            // Save the PDF
-            pdf.save('document.pdf');
-        }
-    });
-}
+        });
+    }
+})
